c9534ce2acaf21bcf00259b2fe8d612a4d0361d9
regenrek <hello@regenrek.at>
2024-09-13 19:33:06 +0200

    update tailwind config

---
+++ tailwind.config.ts
@@ -0,0 +1,24 @@
+import type { Config } from 'tailwindcss'
+import defaultTheme from 'tailwindcss/defaultTheme'
+
+export default <Partial<Config>>{
+  theme: {
+    extend: {
+      colors: {
+        'wildsand': {
+          50: '#f6f6f6',
+          100: '#efefef',
+          200: '#dcdcdc',
+          300: '#bdbdbd',
+          400: '#989898',
+          500: '#7c7c7c',
+          600: '#656565',
+          700: '#525252',
+          800: '#464646',
+          900: '#3d3d3d',
+          950: '#292929'
+        }
+      }
+    }
+  }
+}
